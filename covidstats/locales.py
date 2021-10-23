6c1020b1deb2636636236357d03ef778702ea146
Veselin Stoyanov <veselin1996@gmail.com>
2021-10-23 23:41:03 +0300

    Move locales to lib directory.

--- covidstats/locales.py
+++ covidstats/locales.py
@@ -3,7 +3,7 @@ import i18n
 
 
 def setup_i18n():
-    locales_path = pathlib.Path(__file__).parent.parent.joinpath('config', 'locales').absolute()
+    locales_path = pathlib.Path(__file__).parent.joinpath('config', 'locales').absolute()
     i18n.load_path.append(locales_path)
     i18n.set('filename_format', '{locale}.{format}')
     i18n.set('enable_memoization', True)
